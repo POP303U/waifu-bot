7ef9fcd80d0bfd3e363d684d9e731ad98e65fcf1
POP303U <nicoanton.gering@gmail.com>
2023-07-17 22:53:02 +0200

    [1.0] First release

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import time
 from discord.ext import commands
 import requests
 
@@ -11,7 +12,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Bot is connected as {bot.user.name}')
+    print(f'#------------------------------#')
+    print(f'| Activity: {bot.activity}               |')
+    print(f'| Status: {bot.status}               |')
+    print(f'| Bot is connected as {bot.user.name}    |')
+    print(f'#------------------------------#')
+    print(f'Console:')
+
 
 @bot.event
 async def on_message(message):
@@ -31,14 +38,29 @@ async def die(ctx):
     await ctx.send('you should kys')
 
 @bot.command()
+async def waifu_repeat(ctx, arg1):
+    count = 0
+    while True:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            image_url = data['images'][0]['url']
+            time.sleep(int(arg1))
+            await ctx.send(image_url)
+            print(f'Log: sent image: {image_url}, count: {count}, delay {arg1}')
+            count = count + 1
+        else:
+            await ctx.send('Failed to fetch waifu image.')
+
+@bot.command()
 async def waifu(ctx):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         image_url = data['images'][0]['url']
         await ctx.send(image_url)
+        print(f'Log: sent image: {image_url}')
     else:
         await ctx.send('Failed to fetch waifu image.')
 
 bot.run('')
-
