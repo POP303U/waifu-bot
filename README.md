64afc8588073930a1c00beb3e14dbb3503948707
POP303U <nicoanton.gering@gmail.com>
2023-07-19 22:02:15 +0200

    [1.0] Speed improvements

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # waifu-bot
-Discord waifu.im scraper
+A Discord bot for scraping images from the waifu.pics api.
+
+## How to set up the Bot?
+
+**Step 1:** Create the bot
+
+Go to the Discord Developer Portal (https://discord.com/developers/applications).
+Create a new application and give it a name.
+Navigate to the "Bot" section and click on "Add Bot" to create a bot user.
+Enable the "Presence Intent" and "Server Members Intent" under the "Privileged Gateway Intents" section.
+Copy the bot token for later use.
+
+**Step 2:** Install necessary dependencies:
+
+Install the discord library by running ```pip install discord```
+
+**Step 3:** Use your bot token 
+
+Replace the string in the function bot.run() with the actual bot token you obtained earlier.
+
+**Step 4:** Add the bot to your server
+
+Go to the Discord Developer Portal and click on your application.
+Navigate to the "OAuth2" section and then to "Url Generator".
+Check the box "bot" and then in the new window below "Administrator".
+Use the url that has been generated to add the bot to your server.
+
+**Step 5:** Run the Programm
+
+After you verified that your bot has all permissions and the bot token is correct,
+run the programm and test it out in a server with the bot running by typing: 
+!waifu
+
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import discord
+import discord 
+from discord.ext import commands, tasks
 import time
-from discord.ext import commands
 import requests
 
-url = 'https://api.waifu.im/search'
+waifu_url = 'https://waifu.pics/api/'
+dog_url = 'https://dog.ceo/api/breeds/image/random'
+cat_url = 'https://api.thecatapi.com/v1/images/search'
+duck_url = 'https://random-d.uk/api/v2'
 
 intents = discord.Intents.default()
 intents.message_content = True  # Enable the message content intent
@@ -13,54 +16,154 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'#------------------------------#')
+    print(f'|   waifu-bot [version 1.0]    |')
     print(f'| Activity: {bot.activity}               |')
     print(f'| Status: {bot.status}               |')
     print(f'| Bot is connected as {bot.user.name}    |')
-    print(f'#------------------------------#')
+    print(f'#-------------------------------#')
     print(f'Console:')
-
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
-        return  # Ignore messages sent by the bot itself
+        return  #Ignore messages sent by the bot itself
 
     if 'lmao' in message.content.lower():
         await message.channel.send('not funny')
 
-    if 'what' in message.content.lower():
-        await message.channel.send('negawatt')
-
     await bot.process_commands(message)
 
 @bot.command()
-async def die(ctx):
-    await ctx.send('you should kys')
+async def dog(ctx):
+    response = requests.get(dog_url)
+    data = response.json()
+    if data['status'] == 'success':
+        image_url = data['message']
+        await ctx.send(image_url)
+    else:
+        await ctx.send('Failed to fetch dog image.')
 
 @bot.command()
-async def waifu_repeat(ctx, arg1):
+async def dog_repeat(ctx, arg1):
     count = 0
+    try:
+        limit = int(arg1)
+    except:
+        print("Failed to convert arg1 to int")
     while True:
-        response = requests.get(url)
+        if count == limit:
+            break
+        count = count + 1
+        response = requests.get(dog_url)
+        data = response.json()
+        if data['status'] == 'success':
+            image_url = data['message']
+            await ctx.send(image_url)
+            print(f'Log: sent image: {image_url}, count: {count}')
+            time.sleep(1)
+        else:
+            await ctx.send('Failed to fetch dog image.')
+
+
+@bot.command()
+async def cat(ctx):
+    response = requests.get(cat_url)
+    data = response.json()
+    if response.status_code == 200:
+        image_url = data[0]['url']
+        print(image_url)
+        await ctx.send(image_url)
+    else:
+        await ctx.send('Failed to fetch cat image.')
+
+@bot.command()
+async def cat_repeat(ctx, arg1):
+    count = 0
+    try:
+        limit = int(arg1)
+    except:
+        print("Failed to convert arg1 to int")
+    while True:
+        if count == limit:
+            break
+        count = count + 1
+        response = requests.get(cat_url)
+        data = response.json()
+        if response.status_code == 200:
+            image_url = data[0]['url']
+            await ctx.send(image_url)
+            print(f'Log: sent image: {image_url}, count: {count}')
+            time.sleep(1)
+        else:
+            await ctx.send('Failed to fetch cat image.')
+
+@bot.command()
+async def waifu_repeat(ctx, arg1="sfw", arg2="waifu", arg3="5"):
+    count = 0
+    try:
+        limit = int(arg3)
+    except:
+        print("Failed to convert arg3 to int")
+    print("Loop:")
+    while True:
+        if count == limit:
+            break
+        count = count + 1
+        response = requests.get(waifu_url + arg1 + '/' + arg2)
         if response.status_code == 200:
             data = response.json()
-            image_url = data['images'][0]['url']
-            time.sleep(int(arg1))
+            image_url = data['url']
             await ctx.send(image_url)
-            print(f'Log: sent image: {image_url}, count: {count}, delay {arg1}')
-            count = count + 1
+            print(f'Log: sent image: {image_url}, count: {count}')
+            time.sleep(1)
         else:
             await ctx.send('Failed to fetch waifu image.')
+    print("Loop finished.")
 
 @bot.command()
-async def waifu(ctx):
-    response = requests.get(url)
+async def waifu(ctx, arg1="sfw", arg2="waifu"):
+    response = requests.get(waifu_url + arg1 + '/' + arg2)
     if response.status_code == 200:
         data = response.json()
-        image_url = data['images'][0]['url']
+        image_url = data['url']
         await ctx.send(image_url)
         print(f'Log: sent image: {image_url}')
     else:
         await ctx.send('Failed to fetch waifu image.')
+
+@bot.command()
+async def duck_repeat(ctx, arg1):
+    count = 0
+    try:
+        limit = int(arg1)
+    except:
+        print("Failed to convert arg1 to int")
+    print("Loop:")
+    while True:
+        if count == limit:
+            break
+        count = count + 1
+        response = requests.get(duck_url, '/random')
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+            image_url = data['url']
+            await ctx.send(image_url)
+            print(f'Log: sent image: {image_url}, count: {count}')
+            time.sleep(1)
+        else:
+            await ctx.send('Failed to fetch duck image.')
+    print("Loop finished.")
+
+@bot.command()
+async def duck(ctx):
+    response = requests.get(duck_url, '/random')
+    if response.status_code == 200:
+        data = response.json()
+        image_url = data['url']
+        await ctx.send(image_url)
+        print(f'Log: sent image: {image_url}')
+    else:
+        await ctx.send('Failed to fetch duck image.')
 
 bot.run('')
