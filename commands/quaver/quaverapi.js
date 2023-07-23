fbe24bc41c14e998ce91a897d4e3ffb8d7b27c0b
POP303U <nicoanton.gering@gmail.com>
2023-07-23 14:36:43 +0200

    Unfinished quaver api

---
+++ commands/quaver/quaverapi.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios')
+const user_url = 'https://api.quavergame.com/v1/users/full/POP303U';
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('quaver')
+		.setDescription('Log quaver user info'),
+	execute(interaction) {
+    const data = fetch(user_url)
+      .then(re => re.json)
+      .then(hi => hi['user']['info']['username'])
+		interaction.reply(data);
+	},
+}
+	
